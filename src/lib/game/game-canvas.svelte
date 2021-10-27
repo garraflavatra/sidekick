0d2329c94a56fc6820662afdfb89edcb6e327467
Romein van Buren <romein@vburen.eu>
2021-10-27 14:12:25 +0200

    Custom cursor
    Signed-off-by: Romein van Buren <romein@vburen.eu>

--- src/lib/game/game-canvas.svelte
+++ src/lib/game/game-canvas.svelte
@@ -5,8 +5,12 @@
 <style>
 	.game {
 		overflow: hidden;
-		cursor: none;
+		cursor: url('../../assets/cursor/cursor.cur') 2 2, auto;
 		width: 100vw;
 		height: 100vh;
+		z-index: 200;
+	}
+	.game:hover {
+		cursor: url('../../assets/cursor/cursor.cur') 2 2, auto;
 	}
 </style>
