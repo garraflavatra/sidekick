32f547056a5802c7529d4c05f3d954cf38a409d0
Romein van Buren <romein@vburen.eu>
2021-10-28 10:45:20 +0200

    Use sveltejs/adapter-netlify
    Signed-off-by: Romein van Buren <romein@vburen.eu>

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import preprocess from 'svelte-preprocess';
-import static_adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-netlify';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -7,7 +7,7 @@ const config = {
 
 	kit: {
 		target: '#svelte',
-		adapter: static_adapter()
+		adapter: adapter()
 	}
 };
 
